0df71b5b1ed7efe8fde5526c913db921ee0978d1
Alexdat2000 <alexdat@list.ru>
2020-05-01 10:18:32 +0300

    Added ordered_set template

---
+++ ordered_set.cpp
@@ -0,0 +1,17 @@
+#include <ext/pb_ds/assoc_container.hpp> // Общий файл.
+#include <ext/pb_ds/tree_policy.hpp> // Содержит класс tree_order_statistics_node_update
+#include <ext/pb_ds/detail/standard_policies.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef tree<
+        int,
+        null_type,
+        less<>,
+        rb_tree_tag,
+        tree_order_statistics_node_update>
+        ordered_set;
+
+
+ordered_set lower, upper;
