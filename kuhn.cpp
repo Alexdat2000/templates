318718ab40716e0730175692faa580c032da20cc
Alexdat2000 <s22v_datskovskiy@179.ru>
2021-10-12 23:32:46 +0300

    improved kuhn

--- kuhn.cpp
+++ kuhn.cpp
@@ -1,45 +1,84 @@
 #include <bits/stdc++.h>
 
+#include <random>
+
+#define all(x) (x).begin(), (x).end()
+#define ll long long
+#define endl "\n"
+
 using namespace std;
 
-const int N = 155;
-vector <int> g[N];
-int mt[N];
-bool used[N];
 
-bool kuhn(int v) {
+int n, m;
+const int N = 510;
+vector <pair <int, ll>> g[N];
+bool used[N], used1[N];
+vector <int> pr;
+
+ll ds;
+
+bool dfs(int v) {
     if (used[v])
         return false;
-
     used[v] = true;
-    for (int i : g[v]) {
-        if (mt[i] == -1 || kuhn(mt[i])) {
-            mt[i] = v;
+    for (auto [i, ln] : g[v]) {
+        if (ln > ds)
+            break;
+
+        if (pr[i] == -1) {
+            pr[i] = v;
+            used1[v] = true;
+            return true;
+        }
+    }
+    for (auto [i, ln] : g[v]) {
+        if (ln > ds)
+            break;
+
+        if (dfs(pr[i])) {
+            pr[i] = v;
+            used1[v] = true;
             return true;
         }
     }
     return false;
 }
 
+vector <int> order;
 
-signed main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+bool kuhn() {
+    pr.assign(m, -1);
+    fill(used1, used1 + n, false);
+    for (int v : order) {
+        for (auto [i, ln] : g[v]) {
+            if (ln > ds)
+                break;
 
-    fill(mt, mt + N, -1);
-
-    int n = 150;
-
-    for (int i = 0; i < n; i++) {
-        fill(used, used + N, false);
-        kuhn(i);
-    }
-
-    vector <pair <int, int>> match;
-    for (int i = 0; i < m; i++) {
-        if (mt[i] != -1) {
-            match.emplace_back(mt[i], i);
+            if (pr[i] == -1) {
+                pr[i] = v;
+                used1[v] = true;
+                break;
+            }
         }
     }
+
+    bool run = true;
+    while (run) {
+        run = false;
+        fill(used, used + n, false);
+        for (int i : order) {
+            if (!used1[i] && dfs(i))
+                run = true;
+        }
+    }
+    return !count(used1, used1 + n, false);
+}
+
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie();
+
+
     return 0;
 }
