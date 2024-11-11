d19ca0a04608931167f32666c3fdf3f6a7cfd5ed
Alexey Datskovskiy <alexdat@list.ru>
2024-11-11 20:00:37 +0300

    Update kuhn.cpp
    Make Kuhn usable

--- kuhn.cpp
+++ kuhn.cpp
@@ -9,32 +9,25 @@
 using namespace std;
 
 
-int n, m;
-const int N = 510;
-vector <pair <int, ll>> g[N];
-bool used[N], used1[N];
-vector <int> pr;
+int n, m;  // left half size, right half size
+const int N = 1e5;
+vector <int> g[N];  // n
+bool used[N], used1[N];  // n, n
+int pr[N]; // m
 
-ll ds;
 
 bool dfs(int v) {
     if (used[v])
         return false;
     used[v] = true;
-    for (auto [i, ln] : g[v]) {
-        if (ln > ds)
-            break;
-
+    for (int i : g[v]) {
         if (pr[i] == -1) {
             pr[i] = v;
             used1[v] = true;
             return true;
         }
     }
-    for (auto [i, ln] : g[v]) {
-        if (ln > ds)
-            break;
-
+    for (int i : g[v]) {
         if (dfs(pr[i])) {
             pr[i] = v;
             used1[v] = true;
@@ -44,40 +37,35 @@ bool dfs(int v) {
     return false;
 }
 
-vector <int> order;
-
-bool kuhn() {
-    pr.assign(m, -1);
+int kuhn() {
     fill(used1, used1 + n, false);
-    for (int v : order) {
-        for (auto [i, ln] : g[v]) {
-            if (ln > ds)
-                break;
-
-            if (pr[i] == -1) {
-                pr[i] = v;
-                used1[v] = true;
-                break;
-            }
-        }
-    }
+    fill(pr, pr + m, -1);
+    // for (int v = 0; v < n; v++) {
+    //     for (int i : g[v]) {
+    //         if (pr[i] == -1) {
+    //             pr[i] = v;
+    //             used1[v] = true;
+    //             break;
+    //         }
+    //     }
+    // }
 
     bool run = true;
     while (run) {
         run = false;
         fill(used, used + n, false);
-        for (int i : order) {
+        for (int i = 0; i < n; i++) {
             if (!used1[i] && dfs(i))
                 run = true;
         }
     }
-    return !count(used1, used1 + n, false);
+    return count(used1, used1 + n, true);  // returns matching size
 }
 
 
 signed main() {
     ios_base::sync_with_stdio(false);
-    cin.tie();
+    cin.tie(nullptr);
 
 
     return 0;
